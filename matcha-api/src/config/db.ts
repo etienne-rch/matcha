2c20aa88697bb086db0a3a3f6158c68f09db5450
lamine_s <lamine_s@etna-alternance.net>
2025-08-30 14:27:12 +0200

    cofig ok, passsword

--- matcha-api/src/config/db.ts
+++ matcha-api/src/config/db.ts
@@ -1,11 +1,25 @@
 import mongoose from 'mongoose';
 
-const dbName =
-  process.env.NODE_ENV === 'test'
-    ? process.env.MONGODB_DB_TEST || 'matcha_test'
-    : process.env.MONGODB_DB || 'matcha_dev';
+// const dbName =
+//   process.env.NODE_ENV === 'test'
+//     ? process.env.MONGODB_DB_TEST || 'matcha_test'
+//     : process.env.MONGODB_DB || 'matcha_dev';
 
-const uri = `${process.env.MONGODB_URI}/${dbName}`;
+// const uri = `${process.env.MONGODB_URI}/${dbName}`;
+
+
+// const dbName =
+//   process.env.NODE_ENV === 'test'
+//     ? process.env.MONGODB_DB_TEST || 'matcha_test'
+//     : process.env.MONGODB_DB || 'matcha_dev';
+
+// const uri = `${process.env.MONGODB_URI}/${dbName}`;
+
+const dbName = 'matcha'; // ou 'matcha_test' si tu veux
+
+const uri = `mongodb://localhost:27017/${dbName}`; // <- URI directe
+
+console.log('✅ URI utilisée :', uri);
 
 export const connectDB = async (): Promise<void> => {
   try {
